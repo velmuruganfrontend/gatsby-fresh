dffb190c159468f5e25fb47f73832b322faaf2aa
velmuruganfrontend <velmuruganfrontenddeveloper@gmail.com>
2020-07-26 15:54:35 +0530

    Adding google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+    resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-173543889-1",
+    },
+  }, 
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
